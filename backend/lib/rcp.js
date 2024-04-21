269e7a636c085bfb6a88d550514678f2e03dee1c
Tuan Pham Anh <baabeetaa@gmail.com>
2024-04-21 22:58:33 +0700

    fix akash

--- backend/lib/rcp.js
+++ backend/lib/rcp.js
@@ -1,7 +1,7 @@
 const { parseDate, estimate_future_block_time } = require('./utils.js');
 
 const fetch_proposal = async (chain, proposal_id) => {
-  const url = `http:///a-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/cosmos/gov/v1beta1/proposals/${proposal_id}`;
+  const url = `http://a-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/cosmos/gov/v1beta1/proposals/${proposal_id}`;
   // console.log(url);
   const response = await fetch(url);
   if (response.status === 200) {
@@ -25,21 +25,28 @@ const fetch_proposal = async (chain, proposal_id) => {
   throw new Error("fetch_proposal error");
 }
 
-const fetch_last_proposal_id = async (chain) => {
-  const url = `http:///a-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/cosmos/gov/v1/proposals?pagination.limit=1&pagination.count_total=false&pagination.reverse=true`;
+const fetch_last_proposal_id = async (chain, use_v1beta1 = false) => {
+  const method = use_v1beta1 ? '/cosmos/gov/v1beta1/proposals' : '/cosmos/gov/v1/proposals';
+  const url = `http://a-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net${method}?pagination.limit=1&pagination.count_total=false&pagination.reverse=true`;
   // console.log(url);
   const response = await fetch(url);
   if (response.status === 200) {
     const data = await response.json();
     const proposal_id = parseInt(data["proposals"][0]["id"]);
     return proposal_id;
+  } else {
+    const data = await response.json();
+    if ((response.status === 501) && (data["code"] === 12)) {
+      // { "code": 12, "message": "Not Implemented", "details": [] }
+      return fetch_last_proposal_id(chain, true);
+    }
   }
 
   throw new Error("fetch_proposal error");
 }
 
 const fetch_latest_block_height = async (chain) => {
-  const url = `http:///r-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/status`;
+  const url = `http://r-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/status`;
   const response = await fetch(url);
   if (response.status === 200) {
     const data = await response.json();
@@ -53,7 +60,7 @@ const fetch_future_block_time  = async (chain, future_height) => {
   const diff_block = 100;
 
   /////
-  let response = await fetch(`http:///r-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/status`);
+  let response = await fetch(`http://r-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/status`);
   if (response.status !== 200) {
     throw new Error("fetch status error");
   }
@@ -64,7 +71,7 @@ const fetch_future_block_time  = async (chain, future_height) => {
 
   /////
   const previous_height = current_height - diff_block;
-  response = await fetch(`http:///r-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/block?height=${previous_height}`);
+  response = await fetch(`http://r-${chain}--${process.env.NOTIONAL_API_KEY}.gw.notionalapi.net/block?height=${previous_height}`);
   if (response.status !== 200) {
     throw new Error("fetch status error");
   }
