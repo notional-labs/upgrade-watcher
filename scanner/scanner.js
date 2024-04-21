3a4a15e473d639db3b0d23627e171088aec997f2
Tuan Pham Anh <baabeetaa@gmail.com>
2024-04-21 08:09:14 +0700

    ...

--- scanner/scanner.js
+++ scanner/scanner.js
@@ -34,77 +34,79 @@ const is_matched = (proposal, latest_block_height) => {
   return false;
 }
 
-const start = async (chain) => {
-  console.log(`[${chain}] start....`)
+const start = async (chains) => {
+  for (let chain of chains) {
+    console.log(`[${chain}] start....`)
 
-  let last_id = 0;
-  {
-    const ss = await get_ScanStatus(chain);
-    if (ss == null) {
-      last_id = 1;
-    } else {
-      last_id = ss['last_id'];
+    let last_id = 0;
+    {
+      const ss = await get_ScanStatus(chain);
+      if (ss == null) {
+        last_id = 1;
+      } else {
+        last_id = ss['last_id'];
+      }
     }
-  }
 
-  while (true) {
-    console.log(`[${chain}] scanning ${last_id}`);
-    await sleep(1000);
+    while (true) {
+      console.log(`[${chain}] scanning ${last_id}`);
+      await sleep(1000);
 
-    try {
-      const proposal = await fetch_proposal(chain, last_id);
-      if (proposal == null) {
-        console.log(`[${chain}] proposal ${last_id} is null`);
+      try {
+        const proposal = await fetch_proposal(chain, last_id);
+        if (proposal == null) {
+          console.log(`[${chain}] proposal ${last_id} is null`);
 
-        // to know if this is missing proposal id or not. We'll fetch next 5 proposals.
-        // if one of them exist then it is the missing proposal id.
-        const proposal_1 = await fetch_proposal(chain,last_id + 1);
-        const proposal_2 = await fetch_proposal(chain,last_id + 2);
-        const proposal_3 = await fetch_proposal(chain,last_id + 3);
-        const proposal_4 = await fetch_proposal(chain,last_id + 4);
-        const proposal_5 = await fetch_proposal(chain,last_id + 5);
-        if ((proposal_1 !== null) ||
-          (proposal_2 !== null) ||
-          (proposal_3 !== null) ||
-          (proposal_4 !== null) ||
-          (proposal_5 !== null)) {
-          console.log(`[${chain}] proposal ${last_id} is the missing proposal, continue...`);
-          last_id++;
-          await set_ScanStatus(chain, last_id);
-          continue
+          // to know if this is missing proposal id or not. We'll fetch next 5 proposals.
+          // if one of them exist then it is the missing proposal id.
+          const proposal_1 = await fetch_proposal(chain, last_id + 1);
+          const proposal_2 = await fetch_proposal(chain, last_id + 2);
+          const proposal_3 = await fetch_proposal(chain, last_id + 3);
+          const proposal_4 = await fetch_proposal(chain, last_id + 4);
+          const proposal_5 = await fetch_proposal(chain, last_id + 5);
+          if ((proposal_1 !== null) ||
+            (proposal_2 !== null) ||
+            (proposal_3 !== null) ||
+            (proposal_4 !== null) ||
+            (proposal_5 !== null)) {
+            console.log(`[${chain}] proposal ${last_id} is the missing proposal, continue...`);
+            last_id++;
+            await set_ScanStatus(chain, last_id);
+            continue
+          }
+
+          // stop processing here
+          console.log(`[${chain}] done`);
+          break;
         }
 
-        // stop processing here
-        console.log(`[${chain}] done`);
-        break;
-      }
+        const latest_block_height = await fetch_latest_block_height(chain);
 
-      const latest_block_height = await fetch_latest_block_height(chain);
+        if (is_matched(proposal, latest_block_height)) {
+          console.log(`[${chain}] found proposal matched ${last_id}`);
 
-      if (is_matched(proposal, latest_block_height)) {
-        console.log(`[${chain}] found proposal matched ${last_id}`);
+          const upgrade_height = parseInt(proposal['proposal']['content']['plan']['height']);
+          const estimated_time = await fetch_future_block_time(chain, upgrade_height);
 
-        const upgrade_height = parseInt(proposal['proposal']['content']['plan']['height']);
-        const estimated_time = await fetch_future_block_time(chain, upgrade_height);
-
-        const p = {
-          id: `${chain}_${proposal['proposal']['proposal_id']}`,
-          chain,
-          proposal_id: last_id,
-          status: proposal['proposal']['status'],
-          name: proposal['proposal']['content']['plan']['name'],
-          height: upgrade_height,
-          voting_end_time: parseDate(proposal['proposal']['voting_end_time']),
-          estimated_time: estimated_time,
+          const p = {
+            id: `${chain}_${proposal['proposal']['proposal_id']}`,
+            chain,
+            proposal_id: last_id,
+            status: proposal['proposal']['status'],
+            name: proposal['proposal']['content']['plan']['name'],
+            height: upgrade_height,
+            voting_end_time: parseDate(proposal['proposal']['voting_end_time']),
+            estimated_time: estimated_time,
+          }
+          await put_Proposal(p);
         }
-        await put_Proposal(p);
-      }
 
-      last_id++;
-      await set_ScanStatus(chain, last_id);
-    } catch (e) {
-      console.log(`[${chain}] err when processing, retrying...`);
-      console.log(e.stack);
+        last_id++;
+        await set_ScanStatus(chain, last_id);
+      } catch (e) {
+        console.log(`[${chain}] err when processing, retrying...`);
+        console.log(e.stack);
+      }
     }
   }
 
--- tracker/tracker.js
+++ tracker/tracker.js
@@ -1,60 +1,59 @@
 const { sync, Proposal } = require('../lib/db.js');
 const {fetch_proposal, fetch_latest_block_height, fetch_future_block_time} = require("../lib/rcp.js");
 
-const start = async (chain) => {
-  console.log(`[${chain}] start....`);
+const start = async (chains) => {
+  for (let chain of chains) {
+    console.log(`[${chain}] start....`);
+    ////////////////////////////////
+    console.log("Update status for tracked proposals");
+    let proposals = await Proposal.findAll();
+    // console.log(JSON.stringify(proposals));
 
-  await sync();
-
-  ////////////////////////////////
-  console.log("Update status for tracked proposals");
-  let proposals = await Proposal.findAll();
-  // console.log(JSON.stringify(proposals));
-
-  for (let p of proposals) {
-    try {
-      const proposal = await fetch_proposal(p.chain, p.proposal_id);
-      const status_new = proposal['proposal']['status'];
-      if (p.status !== status_new) {
-        p.status = proposal['proposal']['status'];
-        await p.save();
-      }
-    } catch (e) {
-      console.log(`[${chain}] err when processing, continue...`);
-    }
-  }
-
-  ////////////////////////////////
-  console.log("Remove proposals which no longer needed");
-  // proposals = await Proposal.findAll();
-
-  for (let p of proposals) {
-    try {
-      if (["PROPOSAL_STATUS_REJECTED", "PROPOSAL_STATUS_FAILED"].includes(p.status)) {
-        await p.destroy();
-      } else {
-        // check passed upgrade time
-        const latest_block_height = await fetch_latest_block_height(p.chain);
-        if (latest_block_height >= p.height) {
-          await p.destroy();
+    for (let p of proposals) {
+      try {
+        const proposal = await fetch_proposal(p.chain, p.proposal_id);
+        const status_new = proposal['proposal']['status'];
+        if (p.status !== status_new) {
+          p.status = proposal['proposal']['status'];
+          await p.save();
         }
+      } catch (e) {
+        console.log(`[${chain}] err when processing, continue...`);
       }
-    } catch (e) {
-      console.log(`[${chain}] err when processing, continue...`);
     }
-  }
 
-  ////////////////////////////////
-  console.log("Update estimated upgraded time");
-  proposals = await Proposal.findAll();
+    ////////////////////////////////
+    console.log("Remove proposals which no longer needed");
+    // proposals = await Proposal.findAll();
 
-  for (let p of proposals) {
-    try {
-      const estimated_time = await fetch_future_block_time(chain, p.height);
-      p.estimated_time = estimated_time;
-      await p.save();
-    } catch (e) {
-      console.log(`[${chain}] err when processing, continue...`);
+    for (let p of proposals) {
+      try {
+        if (["PROPOSAL_STATUS_REJECTED", "PROPOSAL_STATUS_FAILED"].includes(p.status)) {
+          await p.destroy();
+        } else {
+          // check passed upgrade time
+          const latest_block_height = await fetch_latest_block_height(p.chain);
+          if (latest_block_height >= p.height) {
+            await p.destroy();
+          }
+        }
+      } catch (e) {
+        console.log(`[${chain}] err when processing, continue...`);
+      }
+    }
+
+    ////////////////////////////////
+    console.log("Update estimated upgraded time");
+    proposals = await Proposal.findAll();
+
+    for (let p of proposals) {
+      try {
+        const estimated_time = await fetch_future_block_time(chain, p.height);
+        p.estimated_time = estimated_time;
+        await p.save();
+      } catch (e) {
+        console.log(`[${chain}] err when processing, continue...`);
+      }
     }
   }
 
